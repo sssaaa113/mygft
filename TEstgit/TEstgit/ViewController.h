719e1348fda123149f37debc0591b6418594b7d0
tarenaios <tarenaios@Macintosh.local>
2015-12-01 10:05:58 +0800

    ertyr

---
+++ TEstgit/TEstgit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEstgit
+//
+//  Created by tarenaios on 15/12/1.
+//  Copyright (c) 2015年 tatrna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TEstgit/TEstgit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TEstgit
+//
+//  Created by tarenaios on 15/12/1.
+//  Copyright (c) 2015年 tatrna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
